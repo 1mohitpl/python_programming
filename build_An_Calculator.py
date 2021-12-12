9f2c9e8a079da783ce29d64440714fc4b0da0eb9
1mohitpl <95656083+1mohitpl@users.noreply.github.com>
2021-12-12 14:16:33 +0530

    Add files via upload

---
+++ build_An_Calculator.py
@@ -0,0 +1,17 @@
+first = input("enter your first number:")
+operator = input("enter your operator(+,_,*,/,%,):")
+second = input("enter your second number:")
+first =int(first)
+second =int(second)
+if operator == "+":
+    print(first + second)
+elif operator =="_":
+    print(first - second)
+elif operator =="*":
+    print(first * second)
+elif operator =="/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)    
+else:
+    print("invalid operation")
