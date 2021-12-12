0e0cfa97763d0907530a4e225405705bd7304420
Mohit Kumar Paul <95656083+1mohitpl@users.noreply.github.com>
2021-12-12 14:50:18 +0530

    Update build_An_Calculator.py

--- build_An_Calculator.py
+++ build_An_Calculator.py
@@ -1,8 +1,13 @@
+                                                             #build an calculator using python
+
+
 first = input("enter your first number:")
 operator = input("enter your operator(+,_,*,/,%,):")
 second = input("enter your second number:")
+
 first =int(first)
 second =int(second)
+
 if operator == "+":
     print(first + second)
 elif operator =="_":
