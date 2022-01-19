92433e1fb46b25c9f0ce84de9ba618550deb9a21
Mohit Kumar Paul <95656083+1mohitpl@users.noreply.github.com>
2022-01-19 23:58:53 +0530

    Update build_An_Calculator.py

--- build_An_Calculator.py
+++ build_An_Calculator.py
@@ -1,6 +1,4 @@
                                                              # build an calculator using python
-
-
 first = input("enter your first number:")
 operator = input("enter your operator(+,_,*,/,%,):")
 second = input("enter your second number:")
